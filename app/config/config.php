6b51f520840b4be3e8d060d11a4d05da7a55c388
Azamat <azamatkurmanbekov@gmail.com>
2019-10-25 11:55:14 +0600

    initial commit

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+return new \Phalcon\Config(array(
+  'application' => array(
+    'controllersDir' => '../app/controllers/',
+    'modelsDir' => '../app/models/',
+    'viewsDir' => '../app/views/',
+    'baseUri' => '//Applications/MAMP/htdocs/PhpUnitTest/'
+  )
+));
